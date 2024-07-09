58886dcd540269bf93ab8e57c9939d3479cea51c
Cris0291 <cris0291@gmail.com>
2024-07-09 09:52:10 -0600

    Add application layer

---
+++ DoteNet/RunApp/RunnApp.Application/Products/Queries/GetProduct/GetProductHandler.cs
@@ -0,0 +1,14 @@
+﻿using ErrorOr;
+using MediatR;
+using RunApp.Domain.Products;
+
+namespace RunnApp.Application.Products.Queries.GetProduct
+{
+    internal class GetProductHandler : IRequestHandler<GetProductQuery, ErrorOr<Product>>
+    {
+        public Task<ErrorOr<Product>> Handle(GetProductQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DoteNet/RunApp/RunnApp.Application/Products/Queries/GetProduct/GetProductQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using ErrorOr;
+using RunApp.Domain.Products;
+
+namespace RunnApp.Application.Products.Queries.GetProduct
+{
+    public record GetProductQuery(Guid ProductId) : IRequest<ErrorOr<Product>>;
+
+
+}
