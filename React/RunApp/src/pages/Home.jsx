a5fdad880b0013e8c7a82733db1ab384cab59b58
Cris0291 <cris0291@gmail.com>
2024-07-14 21:58:19 -0600

    Add Home Component

--- React/RunApp/src/pages/Home.jsx
+++ React/RunApp/src/pages/Home.jsx
@@ -1,20 +1,31 @@
 function Home() {
   return (
-    <div className="flex justify-center mx-auto w-[1000px] border-8 border-sky-500">
-      <div className="card card-side bg-base-100 shadow-xl ">
-        <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-            alt="Movie"
-          />
-        </figure>
-        <div className="card-body ">
-          <h2 className="card-title">New movie is released!</h2>
-          <p>Click the button to watch on Jetflix app.</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Watch</button>
+    <div className="grid grid-cols-[2fr_auto_auto] border-8 border-sky-500">
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
           </div>
         </div>
+      </div>
+
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+        </div>
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
       </div>
     </div>
   );
