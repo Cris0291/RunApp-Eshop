6a9a0e3a6fe0cfcb0e7645ab14ee5b9b81885fe6
Cris0291 <cris0291@gmail.com>
2024-07-18 10:10:25 -0600

    Modified product root aggregate

--- DoteNet/RunApp/RunApp.Domain/ProductAggregate/Product.cs
+++ DoteNet/RunApp/RunApp.Domain/ProductAggregate/Product.cs
@@ -3,13 +3,14 @@ using RunApp.Domain.ProductAggregate.AboutValueType;
 using RunApp.Domain.ProductAggregate.Reviews.ReviewErrors;
 using ErrorOr;
 using RunApp.Domain.ProductAggregate.ProductErrors;
+using System.Collections.Immutable;
 
 namespace RunApp.Domain.Products
 {
     public class Product
     {
         private Product() { }
-        public Guid ProductId { get; set; }
+        public Guid ProductId { get; private set; }
         public string Name { get; private set; }
         public decimal PriceWithDiscount { get; private set; } = 0m;
         public decimal ActualPrice { get; private set; }
@@ -22,35 +23,67 @@ namespace RunApp.Domain.Products
 
         private HashSet<Review> _reviews = new();
         public IReadOnlyCollection<Review> Reviews => _reviews?.ToList();
+        public static List<Error> Errors { get; private set; } = new();
 
-        public static ErrorOr<Product> CreateProduct(string name, decimal price, string description, ICollection<About> bulletpoints)
+        public static ErrorOr<Product> CreateProduct(string name, string description, decimal price, ICollection<string> bulletpoints, decimal priceWithDiscount, string? promotionalText)
         {
-            if (string.IsNullOrEmpty(name)) return ProductError.AllProductsMustHaveAName;
-            if (!bulletpoints.Any())
-            {
-                return ProductError.BulletPointsCollectionShoulNotBeEmpty;
-            }
+            if (string.IsNullOrEmpty(name)) AddError(ProductError.AllProductsMustHaveAName);
+            if (string.IsNullOrEmpty(description)) AddError(ProductError.AllProductsMustHaveADescription);
+            if (!bulletpoints.Any()) AddError(ProductError.BulletPointsCollectionShoulNotBeEmpty);
+            if (Errors.Any()) return CreateErrorListCopy();
+
+            
 
             return new Product
             {
                 Name = name,
                 ActualPrice = price,
                 Description = description,
-                BulletPoints = bulletpoints
+                BulletPoints = bulletpoints.Select(point => new About() { BulletPoint = point }).ToList(),
+                PriceWithDiscount =priceWithDiscount,
+                PromotionalText = promotionalText,
             };
            
 
+        }
+
+        public ErrorOr<Success> UpdateProduct(string name, string description, decimal price, ICollection<string> bulletpoints)
+        {
+            if (string.IsNullOrEmpty(name)) AddError(ProductError.AllProductsMustHaveAName);
+            if (string.IsNullOrEmpty(description)) AddError(ProductError.AllProductsMustHaveADescription);
+            if (!bulletpoints.Any()) AddError(ProductError.BulletPointsCollectionShoulNotBeEmpty);
+            if (Errors.Any()) return CreateErrorListCopy();
+
+            Name = name;
+            ActualPrice = price;
+            Description = description;
+            BulletPoints = bulletpoints.Select(point => new About() { BulletPoint = point }).ToList();
+            
+            return Result.Success;
+        }
+
+        public ErrorOr<Success> AddBulletPoints(IEnumerable<string> points)
+        {
+            if (!points.Any())
+            {
+                AddError(ProductError.BulletPointsCollectionShoulNotBeEmpty);
+                return CreateErrorListCopy();
+            }
+            foreach(string point in points)
+            {
+                BulletPoints.Add(new About() {BulletPoint = point});
+            }
+
+            return Result.Success;
         }
         public ErrorOr<Success> AddReview(string comment, double numStars)
         {
             double minNumOfStars = 1.0;
             //This should be validated in the presentation/application layer as a model validation and not as an invariant
-            if (string.IsNullOrEmpty(comment)) return ReviewError.AllReviewsMustHaveAComment;
+            if (string.IsNullOrEmpty(comment)) AddError(ReviewError.AllReviewsMustHaveAComment);
+            if(numStars < minNumOfStars) AddError(ReviewError.MinimumNunberOfStarsCannotBeLessThanOne);
+            if (Errors.Any()) return CreateErrorListCopy();
 
-            if(numStars < minNumOfStars)
-            {
-                return ReviewError.MinimumNunberOfStarsCannotBeLessThanOne;
-            }
 
             _reviews.Add(new Review { Comment = comment, NumOfStars = numStars});
             return Result.Success;
@@ -70,8 +103,10 @@ namespace RunApp.Domain.Products
         public ErrorOr<Success> AddPriceWithDiscount(decimal priceWithDiscount, string promotionalText)
         {
             decimal maximumDiscount = 0.7m;
-            if (priceWithDiscount < ActualPrice - (ActualPrice * maximumDiscount)) return ProductError.DiscountPricesMustBeMaximum70Percent;
-            if (string.IsNullOrEmpty(promotionalText)) return ProductError.AllPricesWithDiscountMustHaveAPromotionalText;
+            if (priceWithDiscount < ActualPrice - (ActualPrice * maximumDiscount)) AddError(ProductError.DiscountPricesMustBeMaximum70Percent);
+            if (string.IsNullOrEmpty(promotionalText)) AddError(ProductError.AllPricesWithDiscountMustHaveAPromotionalText);
+            if (Errors.Any()) return CreateErrorListCopy();
+
 
             PriceWithDiscount = priceWithDiscount;
             PromotionalText = promotionalText;
@@ -85,6 +120,18 @@ namespace RunApp.Domain.Products
             PromotionalText = null;
 
             return Result.Success;
+        }
+
+        private static void AddError(Error error)
+        {
+            Errors.Add(error);
+        }
+
+        private static List<Error> CreateErrorListCopy()
+        {
+            List<Error> listOfErrors = Errors.ToList();
+            Errors.Clear();
+            return listOfErrors;
         }
     }
 }
--- DoteNet/RunApp/RunApp.Domain/ProductAggregate/ProductErrors/ProductError.cs
+++ DoteNet/RunApp/RunApp.Domain/ProductAggregate/ProductErrors/ProductError.cs
@@ -9,5 +9,6 @@ namespace RunApp.Domain.ProductAggregate.ProductErrors
         public static Error AllProductsMustHaveAName = Error.Validation(code: "AllProductsMustHaveAName", description: "All products must have a name");
         public static Error DiscountPricesMustBeMaximum70Percent = Error.Validation(code: "DiscountPricesMustBeMaximum70Percent", description: "Maximum discount must be of 70% on a product");
         public static Error AllPricesWithDiscountMustHaveAPromotionalText = Error.Validation(code: "AllPricesWithDiscountMustHaveAPromotionalText", description: "All prices with discount must have a promotional text");
+        public static Error AllProductsMustHaveADescription = Error.Validation(code: "AllProductsMustHaveADescription", description: "All products must have a description");
     }
 }
