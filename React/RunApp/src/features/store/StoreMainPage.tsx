c3842b399d5490da6cfaa3cc4f1d3a31cf50a9b4
Cris0291 <cris0291@gmail.com>
2024-08-17 19:53:41 -0600

    Add drop down menu, store main page and svg component

--- React/RunApp/src/features/store/StoreMainPage.tsx
+++ React/RunApp/src/features/store/StoreMainPage.tsx
@@ -3,18 +3,26 @@ import StoreCarousel from './StoreCarousel';
 import StoreBanner from './StoreBanner';
 import StoreSVG from './StoreSVG';
 import classes from './svg.module.css';
-import CarouselBar from './CarouselBar';
+import ProductsGrid from './ProductsGrid';
 
 function StoreMainPage() {
   return (
-    <div style={{ backgroundColor: '#FFFFFF' }}>
+    <div style={{ backgroundColor: '#FFFFFF', paddingTop: 1, paddingBottom: 1 }}>
       <StoreCarousel />
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginBottom: 100,
+        }}
+      >
         <StoreBanner
           MainTitle={'Wait a minute...'}
           Subtitle={'Take a moment to check our app'}
           Body={'Get the body of your dreams was never that easy, get fit the easy way!!!'}
-          color={'#80d0fd'}
+          color={'#000000'}
           width={100}
           Buttontext={'Keep on working out'}
           margin={undefined}
@@ -22,10 +30,21 @@ function StoreMainPage() {
         />
       </div>
 
-      <div style={{ marginTop: 100 }}></div>
-      <CarouselBar />
+      <div style={{ marginBottom: 50 }}>
+        <Title order={1} className={classes.title} ta="center" c="black">
+          Hot Sales!!!
+        </Title>
+        <ProductsGrid />
+      </div>
 
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginBottom: 5,
+        }}
+      >
         <StoreSVG />
       </div>
       <Title order={1} className={classes.title} ta="center">
