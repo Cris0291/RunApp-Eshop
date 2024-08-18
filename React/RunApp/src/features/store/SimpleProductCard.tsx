a5803f4463492b91a166a529e57e0a2a9257c81f
Cris0291 <cris0291@gmail.com>
2024-08-17 19:50:22 -0600

    Add grid and product card component

--- React/RunApp/src/features/store/SimpleProductCard.tsx
+++ React/RunApp/src/features/store/SimpleProductCard.tsx
@@ -9,7 +9,7 @@ import {
   useMantineTheme,
   rem,
 } from '@mantine/core';
-import { IconHeart, IconBookmark, IconShare } from '@tabler/icons-react';
+import { IconHeartFilled, IconBookmarkFilled } from '@tabler/icons-react';
 import classes from './simpleProductCard.module.css';
 
 export function SimpleProductCard() {
@@ -21,31 +21,34 @@ export function SimpleProductCard() {
         <Image
           src="https://m.media-amazon.com/images/I/81XVpnuaxIL._AC_SX425_.jpg"
           alt="Top 50 underrated plants for house decoration"
-          height={250}
+          style={{ objectFit: 'fill' }}
         />
       </Card.Section>
 
       <Badge w="fit-content" variant="light">
         decorations
       </Badge>
-
-      <Text c="blue" fw={500} className={classes.title} mt="xs">
-        Top 50 underrated plants for house decoration
-      </Text>
-      <Rating />
-
+      <Card.Section>
+        <Text c="blue" fw={500} className={classes.title} mt="xs">
+          Top 50 underrated plants for house decoration
+        </Text>
+        <Rating />
+        <Text fw={700} c="black">
+          $ 174.5
+        </Text>
+      </Card.Section>
       <Card.Section className={classes.footer}>
         <Group justify="space-between">
           <Group gap={0}>
             <ActionIcon variant="subtle" color="gray">
-              <IconHeart
+              <IconHeartFilled
                 style={{ width: rem(20), height: rem(20) }}
                 color={theme.colors.red[6]}
                 stroke={1.5}
               />
             </ActionIcon>
             <ActionIcon variant="subtle" color="gray">
-              <IconBookmark
+              <IconBookmarkFilled
                 style={{ width: rem(20), height: rem(20) }}
                 color={theme.colors.yellow[6]}
                 stroke={1.5}
