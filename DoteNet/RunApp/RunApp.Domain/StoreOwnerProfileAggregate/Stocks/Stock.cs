fb126edbcb5d9e476984744228782a69be131a9e
Cris0291 <cris0291@gmail.com>
2024-08-26 09:57:58 -0600

    Add sales, stock and store owner entities

---
+++ DoteNet/RunApp/RunApp.Domain/StoreOwnerProfileAggregate/Stocks/Stock.cs
@@ -0,0 +1,16 @@
+﻿using RunApp.Domain.Products;
+
+namespace RunApp.Domain.StoreOwnerProfileAggregate.Stocks
+{
+    public class Stock
+    {
+        internal Stock() { }
+        public Guid StockId { get; internal set; }
+        public int AddedStock { get; internal set; }
+        public int SoldStock { get; internal set; }
+        public DateTime StockChangeDate { get; internal set; }
+        public Product ProductSold { get; internal set; }
+        public Guid ProductId { get; internal set; }
+        public Guid StoreOwnerProfileId { get; internal set; }
+    }
+}
--- DoteNet/RunApp/RunApp.Domain/StoreOwnerProfileAggregate/StoreOwnerProfile.cs
+++ DoteNet/RunApp/RunApp.Domain/StoreOwnerProfileAggregate/StoreOwnerProfile.cs
@@ -1,8 +1,18 @@
-﻿namespace RunApp.Domain.StoreOwnerProfileAggregate
+﻿using RunApp.Domain.StoreOwnerProfileAggregate.Sales;
+using RunApp.Domain.StoreOwnerProfileAggregate.Stocks;
+
+namespace RunApp.Domain.StoreOwnerProfileAggregate
 {
     public class StoreOwnerProfile
     {
         private StoreOwnerProfile() { }
 
+        public Guid StoreOwnerProfileId { get; private set; }
+        public string StoreName { get; private set; }
+        public int TotalProductsSold { get; private set; }
+        public decimal TotalSalesInCash { get; private set; }
+        public int TotalStock { get; private set; }
+        public  List<Sale> Sales { get; private set; }
+        public List<Stock> Stocks { get; private set; }
     }
 }
