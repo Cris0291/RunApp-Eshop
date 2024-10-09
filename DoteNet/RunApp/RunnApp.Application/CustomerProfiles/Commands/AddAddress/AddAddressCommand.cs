15644dfa373848f510e46a72b27034ea2bf70c1f
Cris0291 <cris0291@gmail.com>
2024-10-09 08:20:15 -0600

    Add address commnad and payment command and also create order event

---
+++ DoteNet/RunApp/RunnApp.Application/CustomerProfiles/Commands/AddAddress/AddAddressCommand.cs
@@ -0,0 +1,10 @@
+﻿using ErrorOr;
+using MediatR;
+using RunApp.Domain.Common.ValueType;
+
+namespace RunnApp.Application.CustomerProfiles.Commands.AddAddress
+{
+    public record AddAddressCommand(Guid UserId, string ZipCode, string Street, string City,
+                                     int BuildingNumber, string Country, string? AlternativeStreet,
+                                     int? AlternativeBuildingNumber) : IRequest<ErrorOr<Address>>;
+}
