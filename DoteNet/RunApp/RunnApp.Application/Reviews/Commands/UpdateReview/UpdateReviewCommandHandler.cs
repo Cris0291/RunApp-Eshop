574feb449d3a0f5b60eda77f4d0115d7a83c9206
Cris0291 <cris0291@gmail.com>
2024-11-06 19:45:58 -0600

    Add update review command

---
+++ DoteNet/RunApp/RunnApp.Application/Reviews/Commands/UpdateReview/UpdateReviewCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using ErrorOr;
+using MediatR;
+using RunApp.Domain.ReviewAggregate;
+
+namespace RunnApp.Application.Reviews.Commands.UpdateReview
+{
+    public class UpdateReviewCommandHandler() : IRequestHandler<UpdateReviewCommand, ErrorOr<Review>>
+    {
+        public async Task<ErrorOr<Review>> Handle(UpdateReviewCommand request, CancellationToken cancellationToken)
+        {
+            
+        }
+    }
+}
