96934e26d9362b49ef4fa5a6cf3320bbbad64cac
Cris0291 <cris0291@gmail.com>
2024-11-11 20:23:27 -0600

    Add review response dto

--- DoteNet/RunApp/Contract/Reviews/Responses/ReviewResponse.cs
+++ DoteNet/RunApp/Contract/Reviews/Responses/ReviewResponse.cs
@@ -3,5 +3,5 @@ using Contracts.Reviews.Requests;
 
 namespace Contracts.Reviews.Responses
 {
-    public record ReviewResponse(string Comment, DateTime Date, string ReviewDescription, RatingResponse RatingResponse);
+    public record ReviewResponse(string Comment, int Rating, DateTime Date, string ReviewDescription);
 }
