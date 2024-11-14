4289fce418a26266621c0daa5a6b3df99fe75757
Cris0291 <cris0291@gmail.com>
2024-11-13 20:43:43 -0600

    Add the update address and payment method use cases

---
+++ DoteNet/RunApp/RunnApp.Application/CustomerProfiles/Commands/UpdateAddress/UpdateAddressCommand.cs
@@ -0,0 +1,8 @@
+﻿using ErrorOr;
+using MediatR;
+using RunApp.Domain.Common.ValueType;
+
+namespace RunnApp.Application.CustomerProfiles.Commands.UpdateAddress
+{
+    public record UpdateAddressCommand(Guid UserId, string ZipCode, string Street, string City, int HouseNumber, string Country, string State) : IRequest<ErrorOr<Address>>;
+}
---
+++ DoteNet/RunApp/RunnApp.Application/CustomerProfiles/Commands/UpdateAddress/UpdateAddressCommnadValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace RunnApp.Application.CustomerProfiles.Commands.UpdateAddress
+{
+    public class UpdateAddressCommnadValidator : AbstractValidator<UpdateAddressCommand>
+    {
+        public UpdateAddressCommnadValidator()
+        {
+            RuleFor(x => x.ZipCode).Matches("^[0-9]{5}(?:-[0-9]{4})?$");
+            RuleFor(x => x.Street).NotEmpty().NotNull();
+            RuleFor(x => x.City).NotEmpty().NotNull();
+            RuleFor(x => x.HouseNumber).GreaterThanOrEqualTo(0);
+            RuleFor(x => x.Country).NotEmpty().NotNull();
+            RuleFor(x => x.State).NotEmpty().NotNull();
+        }
+    }
+}
