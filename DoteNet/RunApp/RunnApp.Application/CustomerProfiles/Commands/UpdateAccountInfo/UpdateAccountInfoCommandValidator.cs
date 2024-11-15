b91ca281d9defc5e9afbd344c12787eb2bc1f910
Cris0291 <cris0291@gmail.com>
2024-11-14 21:12:29 -0600

    Add update account and get user info use cases

---
+++ DoteNet/RunApp/RunnApp.Application/CustomerProfiles/Commands/UpdateAccountInfo/UpdateAccountInfoCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace RunnApp.Application.CustomerProfiles.Commands.UpdateAccountInfo
+{
+    public class UpdateAccountInfoCommandValidator : AbstractValidator<UpdateAccountInfoCommand>
+    {
+        public UpdateAccountInfoCommandValidator()
+        {
+            RuleFor(x => x.Name).NotNull().NotEmpty();
+            RuleFor(x => x.NickName).NotNull().NotEmpty();
+            RuleFor(x => x.Email).Matches("/\\S+@\\S+\\.\\S+/");
+        }
+    }
+}
