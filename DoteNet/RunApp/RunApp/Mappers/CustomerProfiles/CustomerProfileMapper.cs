671cf8de5edb3eda47f577a33666661fff828373
Cris0291 <cris0291@gmail.com>
2024-11-14 21:13:04 -0600

    Modified the customer mapper

---
+++ DoteNet/RunApp/RunApp/Mappers/CustomerProfiles/CustomerProfileMapper.cs
@@ -0,0 +1,26 @@
+﻿using Contracts.CustomerProfile.Response;
+using RunApp.Domain.Common.ValueType;
+using RunApp.Domain.CustomerProfileAggregate;
+
+namespace RunApp.Api.Mappers.CustomerProfiles
+{
+    public static class CustomerProfileMapper
+    {
+        public static AddressResponse FromAddressToAddressResponse(this Address addressRequest)
+        {
+            return new AddressResponse(addressRequest.ZipCode, addressRequest.Street, addressRequest.City, addressRequest.HouseNumber, addressRequest.Country, addressRequest.State);
+        }
+        public static AccountInfoResponse FromCustomerToAccountResponse(this CustomerProfile customerProfile)
+        {
+            return new AccountInfoResponse(customerProfile.Name, customerProfile.Email, customerProfile.NickName);
+        }
+        public static UserInfoResponse FromCustomerToUserInfo(this CustomerProfile customerProfile)
+        {
+            return new UserInfoResponse(customerProfile.Name, customerProfile.Email, customerProfile.NickName, customerProfile.ShippingAdress?.FromAddressToAddressResponse(), customerProfile.PaymentMethod?.FromCardToCardResponse());
+        }
+        public static CardResponse FromCardToCardResponse(this Card card)
+        {
+            return new CardResponse(card.HoldersName, card.CardNumber, card.CVV, card.ExpiryDate);
+        }
+    }
+}
