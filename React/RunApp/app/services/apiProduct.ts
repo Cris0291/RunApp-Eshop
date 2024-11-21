cf7dbf70d424b0db2c8d10b02f5a4fb0c7ecf021
Cris0291 <cris0291@gmail.com>
2024-11-20 19:28:43 -0600

    Add more products to the api product file

--- React/RunApp/app/services/apiProduct.ts
+++ React/RunApp/app/services/apiProduct.ts
@@ -1,8 +1,22 @@
 import axios from "axios"
 import { Product } from "../features/store/product/contracts";
+import { ProductRequestDto, ProductResponseDto } from "../features/profiles/creationcenter/contracts";
+import { headers } from "next/headers";
 
 axios.defaults.baseURL = "http://localhost:5253"; 
 
-export default function GetProduct(query : string){
-    return axios.get<Product>(`api/products${query}`).then(response => response.data)
+export async function GetProduct(query : string, token: string){
+    return axios.get<Product>(`api/products/${query}`, {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    }).then(response => response.data)
+}
+
+export async function CreateProduct({product, token}: {product: ProductRequestDto, token: string}){
+    return axios.post<ProductResponseDto>("api/products", product, {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    }).then(response => response.data)
 }
