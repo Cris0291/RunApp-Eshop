940428c981abd741a8d217cd09709ca7c54123ac
Cris0291 <cris0291@gmail.com>
2024-11-20 19:29:34 -0600

    fix the get product custom hook

--- React/RunApp/app/features/store/product/useGetProductQuery.ts
+++ React/RunApp/app/features/store/product/useGetProductQuery.ts
@@ -1,12 +1,14 @@
-import GetProduct from "@/app/services/apiProduct";
+import {GetProduct} from "@/app/services/apiProduct";
 import { useQuery } from "@tanstack/react-query";
-import { Product } from "./contracts";
+import { useAppSelector } from "@/app/hooks/reduxHooks";
+import { getUserToken } from "../../registration/userSlice";
 
 
 
 export default function useGetProductQuery(query: string){
+    const token = useAppSelector(getUserToken)
     const {isLoading, data: product, error} = useQuery({
-        queryFn: () => GetProduct(query),
+        queryFn: () => GetProduct(query, token),
         queryKey: ["product"]
     });
 
