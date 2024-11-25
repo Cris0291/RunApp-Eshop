ea5cb93a887b6cc4ab6c2d19ede08e63cb8c8fbd
Cris0291 <cris0291@gmail.com>
2024-11-24 21:25:33 -0600

    Add the axios requests for create a new category and createa new price with discount

--- React/RunApp/app/services/apiProduct.ts
+++ React/RunApp/app/services/apiProduct.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { Product } from "../features/store/product/contracts";
-import { ProductRequestDto, ProductResponseDto } from "../features/profiles/creationcenter/contracts";
+import { CategoryResponse, newCategoryDto, newPromotionDto, ProductRequestDto, ProductResponseDto } from "../features/profiles/creationcenter/contracts";
 import { headers } from "next/headers";
 
 axios.defaults.baseURL = "http://localhost:5253"; 
@@ -19,4 +19,29 @@ export async function CreateProduct({product, token}: {product: ProductRequestDt
             "Authorization": `Bearer ${token}`
         }
     }).then(response => response.data)
+}
+
+export async function AddNewDiscount({newDiscount, productId, token}: {newDiscount: newPromotionDto, productId: string, token: string}){
+    return axios.post<ProductResponseDto>(`api/products/${productId}/discounts`, newDiscount, {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    }).then(response => response.data);
+}
+
+export async function AddNewCategory({newCategory, productId, token}: {newCategory: newCategoryDto, productId: string, token: string}){
+    return axios.post<CategoryResponse>(`api/products/${productId}/categories`, newCategory, {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    }).then(response => response.data);
+}
+
+export async function DeleteProduct({productId, token}: {productId: string, token: string}){
+    return axios.delete(`api/products/${productId}`,{
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    }).then(response => response);
+
 }
