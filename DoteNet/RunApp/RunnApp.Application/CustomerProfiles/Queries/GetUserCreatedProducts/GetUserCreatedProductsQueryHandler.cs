3817c5cb0e46bb0a899b4bad418acc555bd78a22
Cris0291 <cris0291@gmail.com>
2024-11-25 20:52:44 -0600

    Add the get created products fucntionality

--- DoteNet/RunApp/RunnApp.Application/CustomerProfiles/Queries/GetUserCreatedProducts/GetUserCreatedProductsQueryHandler.cs
+++ DoteNet/RunApp/RunnApp.Application/CustomerProfiles/Queries/GetUserCreatedProducts/GetUserCreatedProductsQueryHandler.cs
@@ -1,13 +1,28 @@
 ﻿using ErrorOr;
 using MediatR;
+using RunApp.Domain.Products;
+using RunnApp.Application.Common.Interfaces;
+using RunnApp.Application.Products.Queries.GetProducts;
 
 namespace RunnApp.Application.CustomerProfiles.Queries.GetUserCreatedProducts
 {
-    public class GetUserCreatedProductsQueryHandler() : IRequestHandler<GetUserCreatedProductsQuery, ErrorOr<Success>>
+    public class GetUserCreatedProductsQueryHandler(IProductsRepository productsRepository, ICustomerProfileRepository customerProfileRepository, ILeftJoinRepository leftJoinRepository) : IRequestHandler<GetUserCreatedProductsQuery, ErrorOr<IEnumerable<ProductWithMainImage>>>
     {
-        public async Task<ErrorOr<Success>> Handle(GetUserCreatedProductsQuery request, CancellationToken cancellationToken)
+        private readonly IProductsRepository _productsRepository = productsRepository;
+        private readonly ICustomerProfileRepository _customerProfileRepository = customerProfileRepository;
+        private readonly ILeftJoinRepository _leftJoinRepository = leftJoinRepository;
+        public async Task<ErrorOr<IEnumerable<ProductWithMainImage>>> Handle(GetUserCreatedProductsQuery request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var user = await _customerProfileRepository.GetCustomerProfile(request.UserId);
+            if (user == null) return Error.Validation(code: "UserWsNotFound", description: "User was not found");
+
+            if (user.CreatedProducts.Count == 0) return new List<ProductWithMainImage>();
+
+            var createdProducts =  _productsRepository.GetCreatedProducts(user.CreatedProducts);
+            var createdProductsWithImage = _leftJoinRepository.GetProductsWithImage(createdProducts);
+
+            return await _leftJoinRepository.ExecuteQuery(createdProductsWithImage);
+            
         }
     }
 }
