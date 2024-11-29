712b74bac98def13eb267ebf10226b27f23e0e09
Cris0291 <cris0291@gmail.com>
2024-11-28 20:51:54 -0600

    Add the orderSlice

---
+++ React/RunApp/app/features/payment/checkout/contracts.ts
@@ -0,0 +1,27 @@
+import { ProductForCart } from "../shoppingcart/contracts"
+
+export type AddressSettingsForm = {
+    address: string,
+    city: string,
+    state: string,
+    zipcode: string,
+    country: string,
+  }
+
+  export type PaymentSettingsForm = {
+    cardnumber: string,
+    cardname: string,
+    expirydate: string,
+    cvv: string,
+  }
+
+  export type OrderDto = {
+    CardRequest?: PaymentSettingsForm,
+    AddressRequest?: AddressSettingsForm,
+  }
+
+  export type OrderResponse = {
+    OrderId: string,
+    CardRequest?: PaymentSettingsForm,
+    AddressRequest?: AddressSettingsForm,
+  }
---
+++ React/RunApp/app/features/payment/checkout/orderSlice.ts
@@ -0,0 +1,54 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { AddressSettingsForm, OrderDto, OrderResponse, PaymentSettingsForm } from "./contracts"
+import { RootState } from "@/app/utils/store"
+import { AppStartListening } from "@/app/utils/listenerMiddleware"
+import { CreateOrderRequest } from "@/app/services/apiOrders"
+import { addItem, addPendingProduct } from "../shoppingcart/cartSlice"
+
+type OrderState = {
+    currentOrder: OrderDto,
+    currentOrderId?: string,
+}
+
+const initialState : OrderState = {
+    currentOrder: {},
+    currentOrderId: undefined,
+}
+
+export const orderSlice = createSlice({
+    name: "order",
+    initialState,
+    reducers: {
+        createOrder: (state, action: PayloadAction<{address?: AddressSettingsForm, card?: PaymentSettingsForm}>) => {
+            state.currentOrder.AddressRequest = action.payload.address;
+            state.currentOrder.CardRequest = action.payload.card;
+        },
+        addOrder: (state, action: PayloadAction<OrderResponse>) => {
+            state.currentOrder.AddressRequest = action.payload.AddressRequest;
+            state.currentOrder.CardRequest = action.payload.CardRequest;
+            state.currentOrderId = action.payload.OrderId;
+        }
+    }
+})
+
+export const {createOrder, addOrder} = orderSlice.actions
+
+export default orderSlice.reducer;
+
+export const getIsCurrentOrder = (state: RootState) => state.order.currentOrderId !== undefined;
+
+export const createOrderListener = (startAppListening: AppStartListening) => {
+    startAppListening({
+        actionCreator: createOrder,
+        effect: async (action, listenerApi) => {
+            const state = listenerApi.getState();
+            const token = state.user.token;
+
+            const orderResponse = await CreateOrderRequest({orderDto: state.order.currentOrder, token});
+            listenerApi.dispatch(addOrder(orderResponse));
+
+            if(state.cart.pendingProductIfOrderDoesNotExist === undefined) throw new Error("Something unexpected happened. the item you are trying to add to the cart was not found");
+            listenerApi.dispatch(addItem(state.cart.pendingProductIfOrderDoesNotExist));
+        }
+    });
+}
