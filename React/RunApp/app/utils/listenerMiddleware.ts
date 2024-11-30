f13576ea621f2edd7d264efd0005576e20a7a515
Cris0291 <cris0291@gmail.com>
2024-11-29 21:09:22 -0600

    Add the listeners to the store

--- React/RunApp/app/utils/listenerMiddleware.ts
+++ React/RunApp/app/utils/listenerMiddleware.ts
@@ -1,6 +1,7 @@
 import { createListenerMiddleware, addListener, TypedStartListening, TypedAddListener,  ListenerEffectAPI } from "@reduxjs/toolkit";
 import { AppDispatch, RootState } from "./store";
-import { addChangeQuantityListener } from "../features/payment/shoppingcart/cartSlice";
+import { addChangeQuantityListener, addItemListener, deleteItemListener } from "../features/payment/shoppingcart/cartSlice";
+import { createOrderListener } from "../features/payment/checkout/orderSlice";
 
 
 export const listenerMiddleware = createListenerMiddleware();
@@ -14,3 +15,6 @@ export type AppAddListener = TypedAddListener<RootState, AppDispatch>;
 export type AppListenerEffectAPI = ListenerEffectAPI<RootState, AppDispatch>;
 
 addChangeQuantityListener(startAppListening);
+addItemListener(startAppListening);
+deleteItemListener(startAppListening);
+createOrderListener(startAppListening);
--- React/RunApp/app/utils/store.ts
+++ React/RunApp/app/utils/store.ts
@@ -3,12 +3,14 @@ import userReducer from "../features/registration/userSlice";
 import productsQueryReducer from "../features/store/products/productsQuerySlice"
 import cartReducer from "../features/payment/shoppingcart/cartSlice";
 import { listenerMiddleware } from "./listenerMiddleware";
+import orderReducer from "../features/payment/checkout/orderSlice";
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
         productsQuery: productsQueryReducer,
         cart: cartReducer,
+        order: orderReducer,
 
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(listenerMiddleware.middleware),
