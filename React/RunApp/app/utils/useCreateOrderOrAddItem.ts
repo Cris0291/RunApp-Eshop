4b572ace0fbed7388eb9537919457a03737f7250
Cris0291 <cris0291@gmail.com>
2024-12-03 22:22:49 -0600

    Change the implementation of the custom hook

--- React/RunApp/app/utils/useCreateOrderOrAddItem.ts
+++ React/RunApp/app/utils/useCreateOrderOrAddItem.ts
@@ -1,23 +1,21 @@
-import { createOrder, getIsCurrentOrder } from "../features/payment/checkout/orderSlice";
+import { AddressSettingsForm, PaymentSettingsForm } from "../features/payment/checkout/contracts";
+import { createOrder } from "../features/payment/checkout/orderSlice";
 import { addItem, addPendingProduct } from "../features/payment/shoppingcart/cartSlice";
 import { ProductForCart } from "../features/payment/shoppingcart/contracts";
-import { getUserAddress, getUserPaymentMethod } from "../features/registration/userSlice";
 import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
 
 export default function useCreateOrderOrAddItem(){
     const dispatch = useAppDispatch();
 
-    const createOrderOrAddItem = (item: ProductForCart) => {
-        const existOrder = useAppSelector(getIsCurrentOrder);
-        const userAddress = useAppSelector(getUserAddress);
-        const userPaymentMethod = useAppSelector(getUserPaymentMethod);
-
+    const createOrderOrAddItem = (item: ProductForCart,  existOrder: boolean, userAddress: AddressSettingsForm | undefined, userPaymentMethod: PaymentSettingsForm | undefined) => {
         if(existOrder){
             dispatch(addItem(item));
+            console.log("Inside add item route");
         }
         else{
             dispatch(addPendingProduct(item));
             dispatch(createOrder({address: userAddress, card: userPaymentMethod}));
+            console.log("Inside create order route");
         }
     }
 
