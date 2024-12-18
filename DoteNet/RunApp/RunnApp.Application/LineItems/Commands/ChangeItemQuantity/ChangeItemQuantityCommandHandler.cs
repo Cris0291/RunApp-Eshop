8bcfd1fe186080d63c8031603d7ba4b3e4722f1e
Cris0291 <cris0291@gmail.com>
2024-12-17 22:26:40 -0600

    Modified the change item quantity command

--- DoteNet/RunApp/RunnApp.Application/LineItems/Commands/ChangeItemQuantity/ChangeItemQuantityCommandHandler.cs
+++ DoteNet/RunApp/RunnApp.Application/LineItems/Commands/ChangeItemQuantity/ChangeItemQuantityCommandHandler.cs
@@ -5,14 +5,17 @@ using RunnApp.Application.Common.Interfaces;
 
 namespace RunnApp.Application.LineItems.Commands.ChangeItemQuantity
 {
-    public class ChangeItemQuantityCommandHandler(IOrderRepository orderRepository, IUnitOfWorkPattern unitOfWorkPattern) : IRequestHandler<ChangeItemQuantityCommand, ErrorOr<LineItem>>
+    public class ChangeItemQuantityCommandHandler(IOrderRepository orderRepository, IUnitOfWorkPattern unitOfWorkPattern) : IRequestHandler<ChangeItemQuantityCommand, ErrorOr<Success>>
     {
         private readonly IOrderRepository _orderRepository = orderRepository;
         private readonly IUnitOfWorkPattern _unitOfWorkPattern = unitOfWorkPattern; 
-        public async Task<ErrorOr<LineItem>> Handle(ChangeItemQuantityCommand request, CancellationToken cancellationToken)
+        public async Task<ErrorOr<Success>> Handle(ChangeItemQuantityCommand request, CancellationToken cancellationToken)
         {
             var order = await _orderRepository.GetOrder(request.OrderId);
             if (order == null) throw new InvalidOperationException("Order was not found in the database");
+
+            bool isProductAdded = order.CheckProductExistence(request.ProductId);
+            if (!isProductAdded) return Error.NotFound(code: "ItemWasNotFound", description: "Requested item was not found as a part of the order");
 
             var result = order.ChangeItemQuantity(request.Quantity, request.ProductId);
             if (result.IsError) return result.Errors;
