535bd7e9d2e8ec617c264ea022664994cd4f6bd3
Cris0291 <cris0291@gmail.com>
2024-12-31 21:35:45 -0600

    Add the get order query

---
+++ DoteNet/RunApp/RunnApp.Application/Orders/Queries/GetOrder/GetOrderQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using ErrorOr;
+using MediatR;
+using RunApp.Domain.OrderAggregate;
+using RunnApp.Application.Common.Interfaces;
+
+namespace RunnApp.Application.Orders.Queries.GetOrder
+{
+    public class GetOrderQueryHandler(IOrderRepository orderRepository) : IRequestHandler<GetOrderQuery, ErrorOr<OrderWrapperDto>>
+    {
+        private readonly IOrderRepository _orderRepository = orderRepository;
+        public async Task<ErrorOr<OrderWrapperDto>> Handle(GetOrderQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _orderRepository.GetCurrentOrder(request.UserId);
+            if (result != null && result.LineItems.Count == 0) return Error.Validation(code: "CannotHaveAnEmptyOrder", description: "Cannot have an empty order");
+
+            return new OrderWrapperDto { Order = result };
+        }
+    }
+}
