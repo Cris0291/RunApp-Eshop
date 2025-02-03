87def5b2a9ab9a17d81bc52ddf47aa2c3dcb1a84
Cris0291 <cris0291@gmail.com>
2025-02-03 07:03:11 -0600

    Delete two components

--- React/RunApp/src/ui/FailureSpan.tsx
+++
@@ -1,7 +0,0 @@
-export default function FailureSpan(){
-    return <span
-    className="px-2 py-1 rounded-full text-xs font-semibold bg-red-100 text-red-800"
-  >
-    Product was deleted
-  </span>
-}
