a84d762f450a08b1f106e4f79507854a29030f87
Cris0291 <cris0291@gmail.com>
2025-02-05 21:33:00 -0600

    modify  redux store

--- React/RunApp/src/utils/store.ts
+++ React/RunApp/src/utils/store.ts
@@ -1,25 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/registration/userSlice";
-import productsQueryReducer from "../features/store/products/productsQuerySlice"
+import productsQueryReducer from "../features/store/products/productsQuerySlice";
 import cartReducer from "../features/payment/shoppingcart/cartSlice";
 import { listenerMiddleware } from "./listenerMiddleware";
 import orderReducer from "../features/payment/checkout/orderSlice";
-import productReducer from "../features/store/product/productSlice";
 import loginReducer from "../features/login/loginSlice";
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-        productsQuery: productsQueryReducer,
-        product: productReducer,
-        cart: cartReducer,
-        order: orderReducer,
-        login: loginReducer
+  reducer: {
+    user: userReducer,
+    productsQuery: productsQueryReducer,
+    cart: cartReducer,
+    order: orderReducer,
+    login: loginReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().prepend(listenerMiddleware.middleware),
+});
 
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(listenerMiddleware.middleware),
-})
+export type RootState = ReturnType<typeof store.getState>;
 
-export type RootState = ReturnType<typeof store.getState>
-
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
