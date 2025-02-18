e13b9a83334f729784e12e12184a836f1b606b7a
Cris0291 <cris0291@gmail.com>
2025-02-18 06:50:17 -0600

    Modify axios instance

--- React/RunApp/src/services/axiosInstance.ts
+++ React/RunApp/src/services/axiosInstance.ts
@@ -15,16 +15,13 @@ interface RetryQueueItem {
 
 axiosInstance.interceptors.request.use(
   function (config) {
-    console.log("intercepted 1");
     const tokenModelJson = Cookies.get("Token");
     const tokenModel: TokenModel =
       tokenModelJson !== undefined ? JSON.parse(tokenModelJson) : undefined;
     if (tokenModel !== undefined) {
       config.headers.Authorization = `Bearer ${tokenModel.token}`;
-      console.log("intercepted 3");
-      console.log(tokenModel.token);
     }
-    console.log("intercepted 2");
+
     return config;
   },
   function (error) {
@@ -41,20 +38,18 @@ axiosInstance.interceptors.response.use(
     const tokenModel: TokenModel =
       tokenModelJson !== undefined ? JSON.parse(tokenModelJson) : undefined;
     const originalRequest: AxiosRequestConfig = error.config;
-    console.log("response 1");
+
     if (
       error.response &&
       error.response.status === 401 &&
       tokenModel !== undefined
     ) {
-      console.log("refresh 1");
       try {
         console.log("problem 1");
         const data = await refreshAccessToken(
           tokenModel.token,
           tokenModel.refreshToken
         );
-        console.log("problem 2");
 
         const token: TokenModel = {
           token: data.token,
@@ -63,21 +58,18 @@ axiosInstance.interceptors.response.use(
         };
 
         Cookies.set("Token", JSON.stringify(token));
-        console.log("refresh 2");
+
         error.config.headers["Authorization"] = `Bearer ${data.token}`;
 
-        console.log("refresh 3");
         return axiosInstance(originalRequest);
       } catch {
-        console.log("refresh 4");
         Cookies.remove("Session");
         store.dispatch(clearUser());
-        console.log("refresh 5");
+
         //window.location.href = "/";
-        console.log("refresh 6");
       }
     }
-    console.log("response 2");
+
     return Promise.reject(error);
   }
 );
